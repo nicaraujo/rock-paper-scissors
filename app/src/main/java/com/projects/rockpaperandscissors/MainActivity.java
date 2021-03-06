433315deeae603758b0be52bd1eef473b2119010
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-06 16:10:21 -0300

    onClick logic added to the game

--- app/src/main/java/com/projects/rockpaperandscissors/MainActivity.java
+++ app/src/main/java/com/projects/rockpaperandscissors/MainActivity.java
@@ -3,6 +3,7 @@ package com.projects.rockpaperandscissors;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void rockSelect(View view) {
+        optionSelected("rock");
+    }
+    public void paperSelect(View view) {
+        optionSelected("paper");
+    }
+    public void scissorSelect(View view) {
+        optionSelected("scissor");
+    }
+    public void optionSelected(String option) {
+        System.out.println(option);
     }
 }
